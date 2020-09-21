45728e3b92cef21758bd52d39d5e8a336f14d30a
Allard Oelen <oelen@l3s.de>
2020-09-21 17:41:03 +0200

    add dist

---
+++ src/components/TipContainer.js
@@ -0,0 +1,109 @@
+// @flow
+
+import React, { Component } from "react";
+
+import type { T_LTWH } from "../types";
+
+type State = {
+  height: number,
+  width: number
+};
+
+type Props = {
+  children: ?React$Element<*>,
+  style: { top: number, left: number, bottom: number },
+  scrollTop: number,
+  pageBoundingRect: T_LTWH
+};
+
+const clamp = (value, left, right) => Math.min(Math.max(value, left), right);
+
+class TipContainer extends Component<Props, State> {
+  state: State = {
+    height: 0,
+    width: 0
+  };
+
+  componentDidUpdate(nextProps: Props) {
+    if (this.props.children !== nextProps.children) {
+      this.updatePosition();
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(this.updatePosition, 0);
+  }
+
+  updatePosition = () => {
+    const { container } = this.refs;
+
+    if (!container || !container.offsetHeight || !container.offsetWidth) {
+      return;
+    }
+
+    const { offsetHeight, offsetWidth } = container;
+
+    this.setState({
+      height: offsetHeight,
+      width: offsetWidth
+    });
+  };
+
+  render() {
+    const { children, style, scrollTop, pageBoundingRect } = this.props;
+
+    const { height, width } = this.state;
+
+    const isStyleCalculationInProgress = width === 0 && height === 0;
+
+    const shouldMove = style.top - height - 5 < scrollTop;
+
+    const top = shouldMove ? style.bottom + 5 : style.top - height - 5;
+
+    // doesn't work correctly (especially problems when using zoom factors)
+    /*const left = clamp(
+      style.left - width / 2,
+      0,
+      pageBoundingRect.width - width
+    );*/
+    let left = style.left - width / 2;
+    if (left < 0) {
+      left = 0;
+    }
+
+    const childrenWithProps = React.Children.map(children, child =>
+      React.cloneElement(child, {
+        onUpdate: () => {
+          this.setState(
+            {
+              width: 0,
+              height: 0
+            },
+            () => {
+              setTimeout(this.updatePosition, 0);
+            }
+          );
+        },
+        popup: {
+          position: shouldMove ? "below" : "above"
+        }
+      })
+    );
+
+    return (
+      <div
+        className="PdfHighlighter__tip-container"
+        style={{
+          visibility: isStyleCalculationInProgress ? "hidden" : "visible",
+          top,
+          left
+        }}
+        ref="container"
+      >
+        {childrenWithProps}
+      </div>
+    );
+  }
+}
+
+export default TipContainer;
